66df0daeb9274169e4640fabc1fe30fee3b8694d
enock24 <enookimuu@gmail.com>
2020-11-07 12:56:47 +0300

    add form componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
